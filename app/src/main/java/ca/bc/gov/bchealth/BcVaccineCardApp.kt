c43ee27dca802ec413d7bfef2c6ada2e75c8e86a
pinakinkansara <pinakin.k.kansara@gmail.com>
2021-09-20 23:15:57 +0530

    Initial commit

---
+++ app/src/main/java/ca/bc/gov/bchealth/BcVaccineCardApp.kt
@@ -0,0 +1,13 @@
+package ca.bc.gov.bchealth
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * [BcVaccineCardApp]
+ *
+ * @author Pinakin Kansara
+ */
+@HiltAndroidApp
+class BcVaccineCardApp : Application() {
+}
