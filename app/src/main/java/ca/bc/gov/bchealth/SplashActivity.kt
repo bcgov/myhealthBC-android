7f9b88e50f346c35f020a0a99099055714ab6a1b
Amit Metri <amit@freshworks.io>
2021-09-21 19:37:30 +0530

    1. Splash screen implementation:VCCA-19, 2. On-boarding screen implemented:VCCA-20

---
+++ app/src/main/java/ca/bc/gov/bchealth/SplashActivity.kt
@@ -0,0 +1,26 @@
+package ca.bc.gov.bchealth
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import ca.bc.gov.bchealth.ui.cards.CardsFragment
+
+/**
+ * [SplashActivity]
+ *
+ * @author amit metri
+ */
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 3000)
+    }
+}
