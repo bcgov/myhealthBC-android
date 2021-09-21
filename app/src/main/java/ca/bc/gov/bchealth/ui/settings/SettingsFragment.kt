eeeae0ad9ba3b331f8c1f51c6dbfabf533d849e8
Amit Metri <amit@freshworks.io>
2021-09-21 21:56:00 +0530

    1. Settings screen implementation in progress

---
+++ app/src/main/java/ca/bc/gov/bchealth/ui/settings/SettingsFragment.kt
@@ -0,0 +1,28 @@
+package ca.bc.gov.bchealth.ui.settings
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ca.bc.gov.bchealth.R
+import ca.bc.gov.bchealth.databinding.FragmentOnboardingBinding
+import ca.bc.gov.bchealth.databinding.FragmentSettingsBinding
+import ca.bc.gov.bchealth.ui.onboarding.OnBoardingFragment
+import ca.bc.gov.bchealth.utils.viewBindings
+
+/**
+ * [SettingsFragment]
+ *
+ * @author amit metri
+ */
+class SettingsFragment : Fragment(R.layout.fragment_settings) {
+
+    private val binding by viewBindings(FragmentSettingsBinding::bind)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+}
