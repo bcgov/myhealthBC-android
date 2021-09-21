e555abb8a4af2b9b2185bdbbbd08317cb5670fc4
Amit Metri <amit@freshworks.io>
2021-09-21 23:18:17 +0530

    VCBA-49: Settings screen implemented.

--- app/src/main/java/ca/bc/gov/bchealth/ui/setting/SettingFragment.kt
+++ app/src/main/java/ca/bc/gov/bchealth/ui/setting/SettingFragment.kt
@@ -4,17 +4,28 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import ca.bc.gov.bchealth.R
-import dagger.hilt.android.AndroidEntryPoint
+import ca.bc.gov.bchealth.databinding.FragmentSettingBinding
+
+import ca.bc.gov.bchealth.utils.viewBindings
 
 /**
  * [SettingFragment]
  *
- * @author Pinakin Kansara
+ * @author amit metri
  */
-@AndroidEntryPoint
 class SettingFragment : Fragment(R.layout.fragment_setting) {
+
+    private val binding by viewBindings(FragmentSettingBinding::bind)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.txtPrivacyStatement.setOnClickListener {
+            // TODO: 21/09/21 being tracked on https://freshworks.atlassian.net/browse/VCBA-49
+        }
+
+        binding.txtHelp.setOnClickListener {
+            // TODO: 21/09/21 being tracked on https://freshworks.atlassian.net/browse/VCBA-49
+        }
     }
 }
