f7c05a61849885ced0c47cfdb444e690c7538edd
Amit Metri <amit@freshworks.io>
2021-09-27 15:12:41 +0530

    1. De-scoped 'Help' 2. Privacy policy URL launch configured using Chrome Custom Tab 3. Fallback logic has been placed.

--- app/src/main/java/ca/bc/gov/bchealth/ui/setting/SettingFragment.kt
+++ app/src/main/java/ca/bc/gov/bchealth/ui/setting/SettingFragment.kt
@@ -1,11 +1,22 @@
 package ca.bc.gov.bchealth.ui.setting
 
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
+import androidx.browser.customtabs.CustomTabColorSchemeParams
+import androidx.browser.customtabs.CustomTabsClient
+import androidx.browser.customtabs.CustomTabsIntent
+import androidx.browser.customtabs.CustomTabsServiceConnection
+import androidx.browser.customtabs.CustomTabsSession
 import androidx.fragment.app.Fragment
 import ca.bc.gov.bchealth.R
 import ca.bc.gov.bchealth.databinding.FragmentSettingBinding
+import ca.bc.gov.bchealth.utils.toast
 import ca.bc.gov.bchealth.utils.viewBindings
+
 
 /**
  * [SettingFragment]
@@ -14,17 +25,84 @@ import ca.bc.gov.bchealth.utils.viewBindings
  */
 class SettingFragment : Fragment(R.layout.fragment_setting) {
 
+    companion object {
+        private const val CUSTOM_TAB_PACKAGE_NAME = "com.android.chrome"
+    }
+
     private val binding by viewBindings(FragmentSettingBinding::bind)
+
+    private lateinit var customTabsSession: CustomTabsSession
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+        /*
+        * For quick loading of the URL
+        * */
+        val connection: CustomTabsServiceConnection = object : CustomTabsServiceConnection() {
+            override fun onCustomTabsServiceConnected(
+                name: ComponentName,
+                client: CustomTabsClient
+            ) {
+                client.warmup(0L)
+                customTabsSession = client.newSession(null)!!
+                try {
+                    customTabsSession
+                        .mayLaunchUrl(
+                            Uri.parse(getString(R.string.url_privacy_policy)),
+                            null,
+                            null
+                        )
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+
+            }
+
+            override fun onServiceDisconnected(name: ComponentName) {}
+        }
+        CustomTabsClient.bindCustomTabsService(
+            requireContext(),
+            CUSTOM_TAB_PACKAGE_NAME,
+            connection
+        )
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.txtPrivacyStatement.setOnClickListener {
-            // TODO: 21/09/21 being tracked on https://freshworks.atlassian.net/browse/VCBA-49
-        }
+        binding.viewPrivacyStatement.setOnClickListener {
 
-        binding.txtHelp.setOnClickListener {
-            // TODO: 21/09/21 being tracked on https://freshworks.atlassian.net/browse/VCBA-49
+            try {
+                val customTabColorSchemeParams: CustomTabColorSchemeParams =
+                    CustomTabColorSchemeParams.Builder()
+                        .setToolbarColor(resources.getColor(R.color.white, null))
+                        .setSecondaryToolbarColor(resources.getColor(R.color.white, null))
+                        .build()
+
+                val builder: CustomTabsIntent.Builder = CustomTabsIntent.Builder(customTabsSession)
+                val customTabIntent: CustomTabsIntent = builder
+                    .setDefaultColorSchemeParams(customTabColorSchemeParams)
+                    .build()
+
+                customTabIntent.launchUrl(
+                    requireContext(),
+                    Uri.parse(getString(R.string.url_privacy_policy))
+                )
+            } catch (e: Exception) {
+                e.printStackTrace()
+                showURLFallBack()
+            }
+        }
+    }
+
+    private fun showURLFallBack() {
+        val webpage: Uri = Uri.parse(getString(R.string.url_privacy_policy))
+        val intent = Intent(Intent.ACTION_VIEW, webpage)
+        try {
+            startActivity(intent)
+        } catch (e: Exception) {
+            context?.toast(getString(R.string.no_app_found))
         }
     }
 }
