e491837f9cff693fb49339352ffda5ee63ed9e1e
Amit Metri <amit@freshworks.io>
2021-11-22 17:34:27 +0530

    1. spotless applied

--- app/src/main/java/ca/bc/gov/bchealth/utils/Extensions.kt
+++ app/src/main/java/ca/bc/gov/bchealth/utils/Extensions.kt
@@ -11,7 +11,6 @@ import android.net.NetworkCapabilities
 import android.net.Uri
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import android.webkit.URLUtil
 import android.widget.Toast
 import androidx.browser.customtabs.CustomTabColorSchemeParams
 import androidx.browser.customtabs.CustomTabsIntent
