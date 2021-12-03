b62c3eddbf116581edbb00bf9edf32274c447d10
Amit Metri <amit@freshworks.io>
2021-12-04 00:25:50 +0530

    - added encrypted preferences.

--- app/src/main/java/ca/bc/gov/bchealth/di/DataStoreModule.kt
+++ app/src/main/java/ca/bc/gov/bchealth/di/DataStoreModule.kt
@@ -1,6 +1,10 @@
 package ca.bc.gov.bchealth.di
 
 import android.content.Context
+import android.content.SharedPreferences
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKey
+import ca.bc.gov.bchealth.BuildConfig
 import ca.bc.gov.bchealth.datasource.DataStoreRepo
 import dagger.Module
 import dagger.Provides
@@ -20,5 +24,25 @@ class DataStoreModule {
 
     @Provides
     @Singleton
-    fun providesDataStore(@ApplicationContext context: Context) = DataStoreRepo(context = context)
+    fun providesDataStore(sharedPreferences: SharedPreferences) =
+        DataStoreRepo(sharedPreferences)
+
+    @Provides
+    @Singleton
+    fun provideEncryptedPreferences(@ApplicationContext context: Context): SharedPreferences {
+
+        // Step 1: Create or retrieve the Master Key for encryption/decryption
+        val masterKey = MasterKey.Builder(context, MasterKey.DEFAULT_MASTER_KEY_ALIAS)
+            .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
+            .build()
+
+        // Step 2: Initialize/open an instance of EncryptedSharedPreferences
+        return EncryptedSharedPreferences.create(
+            context,
+            BuildConfig.APPLICATION_ID + "_preferences",
+            masterKey,
+            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+        )
+    }
 }
