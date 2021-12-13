ce8e404cc4381028a7a98961b119351a8cb37147
Amit Metri <amit@freshworks.io>
2021-12-13 12:35:40 +0530

    - updated on-boarding screens design - refactored new-feature screen code

--- app/src/main/java/ca/bc/gov/bchealth/ui/onboarding/OnBoardingHealthRecordsFragment.kt
+++ app/src/main/java/ca/bc/gov/bchealth/ui/onboarding/OnBoardingHealthRecordsFragment.kt
@@ -3,4 +3,4 @@ package ca.bc.gov.bchealth.ui.onboarding
 import androidx.fragment.app.Fragment
 import ca.bc.gov.bchealth.R
 
-class OnboardingHealthPassesFragment : Fragment(R.layout.fragment_onboarding_health_passes)
+class OnBoardingHealthRecordsFragment : Fragment(R.layout.fragment_on_boarding_health_records)
