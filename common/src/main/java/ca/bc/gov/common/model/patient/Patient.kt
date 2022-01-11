7e0adeb5c2a27e2240b0c8af88d5e22c7c9cae67
Amit Metri <amit@freshworks.io>
2022-01-11 23:22:44 +0530

    - added test details

--- common/src/main/java/ca/bc/gov/common/model/patient/Patient.kt
+++ common/src/main/java/ca/bc/gov/common/model/patient/Patient.kt
@@ -1,14 +1,17 @@
 package ca.bc.gov.common.model.patient
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 import java.time.Instant
 
 /**
  * @author Pinakin Kansara
  */
+@Parcelize
 data class Patient(
     val id: Long = 0,
     val firstName: String,
     val lastName: String,
     val dateOfBirth: Instant,
     val phn: String? = null,
-)
+): Parcelable
