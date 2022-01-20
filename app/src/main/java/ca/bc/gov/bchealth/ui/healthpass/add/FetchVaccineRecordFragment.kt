39194c7da2361f1032d0f122cf48b50edf669845
Amit Metri <amit@freshworks.io>
2022-01-20 18:40:47 +0530

    -added data mismatch error handling

--- app/src/main/java/ca/bc/gov/bchealth/ui/healthpass/add/FetchVaccineRecordFragment.kt
+++ app/src/main/java/ca/bc/gov/bchealth/ui/healthpass/add/FetchVaccineRecordFragment.kt
@@ -82,8 +82,8 @@ class FetchVaccineRecordFragment : Fragment(R.layout.fragment_fetch_vaccine_reco
 
                     if (uiState.isError) {
                         requireContext().showError(
-                            getString(R.string.error),
-                            getString(R.string.error_message)
+                            getString(uiState.errorData.title),
+                            getString(uiState.errorData.message)
                         )
                     }
 
@@ -117,13 +117,13 @@ class FetchVaccineRecordFragment : Fragment(R.layout.fragment_fetch_vaccine_reco
                     getString(R.string.phn_should_be_10_digit)
                 ) &&
                 this.validateDatePickerData(
-                    binding.tipDob,
-                    getString(R.string.dob_required)
-                ) &&
+                        binding.tipDob,
+                        getString(R.string.dob_required)
+                    ) &&
                 this.validateDatePickerData(
-                    binding.tipDov,
-                    getString(R.string.dov_required)
-                )
+                        binding.tipDov,
+                        getString(R.string.dov_required)
+                    )
             ) {
 
                 viewModel.fetchVaccineRecord(phn, dob, dov)
