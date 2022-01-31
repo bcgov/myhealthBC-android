2d5f721ab252e13c7c6e66ad953fa6b1a9e6f37b
rashmibambhania <rashmi@freshworks.io>
2022-01-31 15:42:02 +0530

    span personal record card, show login card if token is expired

--- app/src/main/java/ca/bc/gov/bchealth/ui/healthrecord/HealthRecordsAdapter.kt
+++ app/src/main/java/ca/bc/gov/bchealth/ui/healthrecord/HealthRecordsAdapter.kt
@@ -5,39 +5,83 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import ca.bc.gov.bchealth.databinding.ItemHealthRecordHeaderBinding
 import ca.bc.gov.bchealth.databinding.ItemHealthRecordMemberBinding
 
 /**
  * @author Pinakin Kansara
  */
+
+private const val ITEM_VIEW_TYPE_HEADER = 0
+private const val ITEM_VIEW_TYPE_MEMBER = 1
+
 class HealthRecordsAdapter(
     private val itemClickListener: ItemClickListener
-) : ListAdapter<PatientHealthRecord, HealthRecordsAdapter.ViewHolder>(
+) : ListAdapter<PatientHealthRecord, RecyclerView.ViewHolder>(
     PatientHealthRecordDiffCallBack()
 ) {
 
-    class ViewHolder(val binding: ItemHealthRecordMemberBinding) :
+    class MemberViewHolder(val binding: ItemHealthRecordMemberBinding) :
         RecyclerView.ViewHolder(binding.root)
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = ItemHealthRecordMemberBinding.inflate(
-            LayoutInflater.from(parent.context), parent, false
-        )
-        return ViewHolder(view)
+    class HeaderViewHolder(val binding: ItemHealthRecordHeaderBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when (viewType) {
+            ITEM_VIEW_TYPE_HEADER -> {
+                val binding = ItemHealthRecordHeaderBinding.inflate(
+                    LayoutInflater.from(parent.context), parent, false
+                )
+                HeaderViewHolder(binding)
+            }
+            ITEM_VIEW_TYPE_MEMBER -> {
+                val binding = ItemHealthRecordMemberBinding.inflate(
+                    LayoutInflater.from(parent.context), parent, false
+                )
+                MemberViewHolder(binding)
+            }
+            else -> throw ClassCastException("Unknown viewType $viewType")
+        }
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val record = getItem(position)
-        val totalRecords: String = if (record.totalRecord == 1)
-            "${record.totalRecord} record"
-        else
-            "${record.totalRecord} records"
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when(holder) {
+            is MemberViewHolder -> {
+                val record = getItem(position)
+                val totalRecords: String = if (record.totalRecord == 1)
+                    "${record.totalRecord} record"
+                else
+                    "${record.totalRecord} records"
 
-        holder.binding.tvNumberOfHealthRecords.text = totalRecords
-        holder.binding.tvMemberName.text = record.name
+                holder.binding.tvNumberOfHealthRecords.text = totalRecords
+                holder.binding.tvMemberName.text = record.name
 
-        holder.itemView.setOnClickListener {
-            itemClickListener.onItemClick(record)
+                holder.itemView.setOnClickListener {
+                    itemClickListener.onItemClick(record)
+                }
+            }
+            is HeaderViewHolder -> {
+                val record = getItem(position)
+                val totalRecords: String = if (record.totalRecord == 1)
+                    "${record.totalRecord} record"
+                else
+                    "${record.totalRecord} records"
+
+                holder.binding.tvNumberOfHealthRecords.text = totalRecords
+                holder.binding.tvMemberName.text = record.name
+
+                holder.itemView.setOnClickListener {
+                    itemClickListener.onItemClick(record)
+                }
+            }
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return when (position) {
+            0 -> ITEM_VIEW_TYPE_HEADER
+            else -> ITEM_VIEW_TYPE_MEMBER
         }
     }
 }
--- app/src/main/java/ca/bc/gov/bchealth/ui/healthrecord/individual/IndividualHealthRecordFragment.kt
+++ app/src/main/java/ca/bc/gov/bchealth/ui/healthrecord/individual/IndividualHealthRecordFragment.kt
@@ -36,6 +36,7 @@ class IndividualHealthRecordFragment : Fragment(R.layout.fragment_individual_hea
     private val viewModel: IndividualHealthRecordViewModel by viewModels()
     private lateinit var vaccineRecordsAdapter: VaccineRecordsAdapter
     private lateinit var testRecordsAdapter: TestRecordsAdapter
+    private lateinit var hiddenHealthRecordAdapter: HiddenHealthRecordAdapter
     private lateinit var concatAdapter: ConcatAdapter
     private val args: IndividualHealthRecordFragmentArgs by navArgs()
     private var patientId: Long = -1L
@@ -119,7 +120,8 @@ class IndividualHealthRecordFragment : Fragment(R.layout.fragment_individual_hea
             canDeleteRecord = false
         )
 
-        concatAdapter = ConcatAdapter(vaccineRecordsAdapter, testRecordsAdapter)
+        hiddenHealthRecordAdapter = HiddenHealthRecordAdapter { onBCSCLoginClick() }
+        concatAdapter = ConcatAdapter(hiddenHealthRecordAdapter, vaccineRecordsAdapter, testRecordsAdapter)
         binding.rvHealthRecords.adapter = concatAdapter
         binding.rvHealthRecords.layoutManager = LinearLayoutManager(requireContext())
     }
@@ -235,5 +237,9 @@ class IndividualHealthRecordFragment : Fragment(R.layout.fragment_individual_hea
             queueITEngine.run(requireActivity())
         } catch (e: Exception) {
         }
+    }
+
+    private fun onBCSCLoginClick() {
+        //Do nothing
     }
 }
