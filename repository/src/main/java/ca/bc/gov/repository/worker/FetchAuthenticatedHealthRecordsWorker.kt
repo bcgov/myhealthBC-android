7fe0f7837f25344fbdbb6320d97b7a1d8f652519
rashmibambhania <rashmi@freshworks.io>
2022-03-04 11:32:15 +0530

    notification icon and message updated

--- repository/src/main/java/ca/bc/gov/repository/worker/FetchAuthenticatedHealthRecordsWorker.kt
+++ repository/src/main/java/ca/bc/gov/repository/worker/FetchAuthenticatedHealthRecordsWorker.kt
@@ -8,12 +8,9 @@ import ca.bc.gov.common.R
 import ca.bc.gov.repository.FetchTestResultRepository
 import ca.bc.gov.repository.FetchVaccineRecordRepository
 import ca.bc.gov.repository.MedicationRecordRepository
-import ca.bc.gov.repository.PatientWithTestResultRepository
 import ca.bc.gov.repository.PatientWithVaccineRecordRepository
 import ca.bc.gov.repository.bcsc.BcscAuthRepo
 import ca.bc.gov.repository.di.IoDispatcher
-import ca.bc.gov.repository.labtest.LabOrderRepository
-import ca.bc.gov.repository.labtest.LabTestRepository
 import ca.bc.gov.repository.patient.PatientRepository
 import ca.bc.gov.repository.utils.NotificationHelper
 import dagger.assisted.Assisted
@@ -32,13 +29,10 @@ class FetchAuthenticatedHealthRecordsWorker @AssistedInject constructor(
     private val fetchTestResultRepository: FetchTestResultRepository,
     private val bcscAuthRepo: BcscAuthRepo,
     private val patientWithVaccineRecordRepository: PatientWithVaccineRecordRepository,
-    private val patientWithTestResultRepository: PatientWithTestResultRepository,
     private val medicationRecordRepository: MedicationRecordRepository,
     @IoDispatcher private val dispatcher: CoroutineDispatcher,
     private val patientRepository: PatientRepository,
-    private val notificationHelper: NotificationHelper,
-    private val labOrderRepository: LabOrderRepository,
-    private val labTestRepository: LabTestRepository
+    private val notificationHelper: NotificationHelper
 ) : CoroutineWorker(context, workerParams) {
 
     override suspend fun doWork(): Result {
@@ -52,13 +46,11 @@ class FetchAuthenticatedHealthRecordsWorker @AssistedInject constructor(
     }
 
     private suspend fun fetchAuthRecords(patientId: Long) {
-        val successApiMsgList = arrayListOf<String>()
-        val failApiMsgList = arrayListOf<String>()
+        var isApiFailed = false
 
         val authParameters = bcscAuthRepo.getAuthParameters()
         notificationHelper.showNotification(
-            context.getString(R.string.notification_title_while_fetching_data),
-            context.getString(R.string.notification_message_while_fetching_data)
+            context.getString(R.string.notification_title_while_fetching_data)
         )
         try {
             withContext(dispatcher) {
@@ -72,21 +64,19 @@ class FetchAuthenticatedHealthRecordsWorker @AssistedInject constructor(
                     )
                 }
             }
-            successApiMsgList.add(context.getString(R.string.vaccine_records))
         } catch (e: Exception) {
-            failApiMsgList.add(context.getString(R.string.vaccine_records))
+            isApiFailed = true
             e.printStackTrace()
         }
         try {
             withContext(dispatcher) {
-                fetchTestResultRepository.fetchCovidTestRecord(
+                fetchTestResultRepository.fetchAuthenticatedTestRecord(
                     patientId, authParameters.first,
                     authParameters.second
                 )
             }
-            successApiMsgList.add(context.getString(R.string.covid_test_result))
         } catch (e: Exception) {
-            failApiMsgList.add(context.getString(R.string.covid_test_result))
+            isApiFailed = true
             e.printStackTrace()
         }
         try {
@@ -97,56 +87,14 @@ class FetchAuthenticatedHealthRecordsWorker @AssistedInject constructor(
                     authParameters.second
                 )
             }
-            successApiMsgList.add(context.getString(R.string.medication_records))
         } catch (e: Exception) {
-            failApiMsgList.add(context.getString(R.string.medication_records))
+            isApiFailed = true
         }
 
-        try {
-            withContext(dispatcher) {
-                val response =
-                    labOrderRepository.fetchLabOrders(authParameters.first, authParameters.second)
-                labOrderRepository.delete(patientId)
-                response.forEach {
-                    it.labOrder.patientId = patientId
-                    labOrderRepository.insert(it.labOrder)
-                    labTestRepository.insert(it.labTests)
-                }
-            }
-            successApiMsgList.add(context.getString(R.string.lab_orders))
-        } catch (e: Exception) {
-            failApiMsgList.add(context.getString(R.string.lab_orders))
+        if(isApiFailed) {
+            notificationHelper.updateNotification(context.getString(R.string.notification_title_on_failed))
+        } else {
+            notificationHelper.updateNotification(context.getString(R.string.notification_title_on_success))
         }
-
-        val notificationMsg = prepareNotificationMsg(successApiMsgList, failApiMsgList)
-        notificationHelper.updateNotification(
-            context.getString(R.string.notification_title_fetching_records_completed),
-            notificationMsg
-        )
-    }
-
-    private fun prepareNotificationMsg(
-        successApiMsgList: ArrayList<String>,
-        failApiMsgList: ArrayList<String>
-    ): String {
-        val notificationMsg = StringBuilder()
-        if (successApiMsgList.isNotEmpty()) {
-            notificationMsg.append(context.getString(R.string.retrieving))
-            notificationMsg.append(successApiMsgList.first())
-            successApiMsgList.subList(1, successApiMsgList.size).forEach {
-                notificationMsg.append(",").append(it)
-            }
-            notificationMsg.append(context.getString(R.string.successful))
-        }
-        if (failApiMsgList.isNotEmpty()) {
-            notificationMsg.append(context.getString(R.string.retrieving))
-            notificationMsg.append(failApiMsgList.first())
-            failApiMsgList.subList(1, failApiMsgList.size).forEach {
-                notificationMsg.append(",").append(it)
-            }
-            notificationMsg.append(context.getString(R.string.failed))
-        }
-
-        return notificationMsg.toString()
     }
 }
