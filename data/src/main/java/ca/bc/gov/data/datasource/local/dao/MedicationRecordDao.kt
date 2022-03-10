db33c353469a35090bec8194565ae35cc908ca6d
Amit Metri <amit@freshworks.io>
2022-03-10 15:26:48 +0530

    -bugfix on merging of non-bcsc records with bcsc records.

--- data/src/main/java/ca/bc/gov/data/datasource/local/dao/MedicationRecordDao.kt
+++ data/src/main/java/ca/bc/gov/data/datasource/local/dao/MedicationRecordDao.kt
@@ -20,9 +20,6 @@ interface MedicationRecordDao : BaseDao<MedicationRecordEntity> {
     @Query("SELECT * FROM medication_record WHERE id = :medicationRecordId")
     suspend fun getMedicationWithSummaryAndPharmacy(medicationRecordId: Long): MedicationWithSummaryAndPharmacy?
 
-    @Query("DELETE FROM medication_record WHERE patient_id = :patientId AND data_source = 'BCSC'")
-    suspend fun deleteAuthenticatedMedicationRecords(patientId: Long): Int
-
-    @Query("SELECT COUNT(*) FROM medication_record WHERE patient_id = :patientId ")
-    suspend fun getCountOfMedicationRecords(patientId: Long): Long
+    @Query("DELETE FROM medication_record WHERE patient_id = :patientId")
+    suspend fun deletePatientMedicationRecords(patientId: Long): Int
 }
