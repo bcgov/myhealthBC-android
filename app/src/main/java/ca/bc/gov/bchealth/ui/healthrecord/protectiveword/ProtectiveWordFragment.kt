aa97c5bdebe9eadbe96bf8248069bcdfaa75f3c2
rashmibambhania <rashmi@freshworks.io>
2022-03-11 15:41:23 +0530

    code refactoring and use enum instead of boolean

--- app/src/main/java/ca/bc/gov/bchealth/ui/healthrecord/protectiveword/ProtectiveWordFragment.kt
+++ app/src/main/java/ca/bc/gov/bchealth/ui/healthrecord/protectiveword/ProtectiveWordFragment.kt
@@ -43,13 +43,6 @@ class ProtectiveWordFragment : Fragment(R.layout.fragment_protective_word) {
 
         binding.btnContinue.setOnClickListener {
             viewModel.fetchMedicationRecords(args.patientId, binding.etProtectiveWord.text.toString())
-            // if(viewModel.isProtectiveWordValid(binding.etProtectiveWord.text.toString())) {
-            //     viewModel.saveProtectiveWord(binding.etProtectiveWord.text.toString())
-            //     // viewModel.clearIsProtectiveWordRequired()
-            //     findNavController().popBackStack()
-            // } else {
-            //     binding.etProtectiveWord.error = "Invalid protective word. Try again."
-            // }
         }
         binding.btnCancel.setOnClickListener {
             findNavController().popBackStack()
