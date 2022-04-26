869089bb42f63b2412dd8ad6f9b1c829b6d61b9d
Amit Metri <amit@freshworks.io>
2022-04-26 15:34:22 +0530

    -bugfix happ-745

--- app/src/main/java/ca/bc/gov/bchealth/ui/healthrecord/HealthRecordsViewModel.kt
+++ app/src/main/java/ca/bc/gov/bchealth/ui/healthrecord/HealthRecordsViewModel.kt
@@ -3,6 +3,7 @@ package ca.bc.gov.bchealth.ui.healthrecord
 import androidx.lifecycle.ViewModel
 import ca.bc.gov.bchealth.model.mapper.toUiModel
 import ca.bc.gov.common.model.AuthenticationStatus
+import ca.bc.gov.repository.bcsc.BcscAuthRepo
 import ca.bc.gov.repository.patient.PatientRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.map
@@ -13,11 +14,18 @@ import javax.inject.Inject
  */
 @HiltViewModel
 class HealthRecordsViewModel @Inject constructor(
-    repository: PatientRepository
+    repository: PatientRepository,
+    private val bcscAuthRepo: BcscAuthRepo
 ) : ViewModel() {
 
     val patientHealthRecords = repository.patientHealthRecords.map { records ->
         records.map { record ->
+            record.patientDto.authenticationStatus =
+                if (bcscAuthRepo.checkSession()) {
+                    AuthenticationStatus.AUTHENTICATED
+                } else {
+                    AuthenticationStatus.AUTHENTICATION_EXPIRED
+                }
             record.toUiModel()
         }
     }
