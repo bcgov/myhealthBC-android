4e18fc5d3ec66de093159793bd10350f3982d377
Amit Metri <amit@freshworks.io>
2022-06-21 23:08:43 +0530

    -added health visit storage logic

---
+++ data/src/main/java/ca/bc/gov/data/datasource/local/dao/HealthVisitsDao.kt
@@ -0,0 +1,15 @@
+package ca.bc.gov.data.datasource.local.dao
+
+import androidx.room.Dao
+import androidx.room.Query
+import ca.bc.gov.data.datasource.local.entity.healthvisits.HealthVisitEntity
+
+/*
+* Created by amit_metri on 21,June,2022
+*/
+@Dao
+interface HealthVisitsDao : BaseDao<HealthVisitEntity> {
+
+    @Query("DELETE FROM health_visits WHERE patient_id = :patientId")
+    suspend fun delete(patientId: Long): Int
+}
---
+++ data/src/main/java/ca/bc/gov/data/datasource/local/entity/healthvisits/HealthVisitEntity.kt
@@ -0,0 +1,45 @@
+package ca.bc.gov.data.datasource.local.entity.healthvisits
+
+import androidx.room.ColumnInfo
+import androidx.room.Embedded
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+import ca.bc.gov.common.model.DataSource
+import ca.bc.gov.data.datasource.local.entity.PatientEntity
+import java.time.Instant
+
+/*
+* Created by amit_metri on 21,June,2022
+*/
+@Entity(
+    tableName = "health_visits",
+    foreignKeys = [
+        ForeignKey(
+            entity = PatientEntity::class,
+            parentColumns = ["id"],
+            childColumns = ["patient_id"],
+            onDelete = ForeignKey.CASCADE,
+            onUpdate = ForeignKey.CASCADE
+        )
+    ]
+)
+data class HealthVisitEntity(
+    // Defined own primary key("health_visit_id") because "id" is set as nullable from the API specs
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "health_visit_id")
+    val healthVisitId: Long = 0,
+    @ColumnInfo(name = "patient_id")
+    val patientId: Long,
+    @ColumnInfo(name = "id")
+    val id: String? = null,
+    @ColumnInfo(name = "encounter_date")
+    val encounterDate: Instant?,
+    @ColumnInfo(name = "specialty_description")
+    val specialtyDescription: String? = null,
+    @ColumnInfo(name = "practitioner_name")
+    val practitionerName: String? = null,
+    @Embedded val clinic: Clinic? = null,
+    @ColumnInfo(name = "data_source")
+    val dataSource: DataSource = DataSource.BCSC
+)
