af0e3131d03cae73696e60c8e096cc9a0d7f1d5b
bsavoini-fw <bruno@freshworks.io>
2022-07-19 14:31:11 -0700

    fix: Update BaseDataBaseTest

--- data/src/androidTest/java/ca/bc/gov/data/local/BaseDataBaseTest.kt
+++ data/src/androidTest/java/ca/bc/gov/data/local/BaseDataBaseTest.kt
@@ -78,7 +78,7 @@ abstract class BaseDataBaseTest {
     }
 
     protected fun getLabOrder(
-        id: String = "ccee5028-6d51-4474-ce18-08d91c31b95b",
+        id: Long = 1,
         patientId: Long = 1,
         collectionDateTime: Instant = Instant.now(),
         reportAvailable: Boolean = false,
@@ -203,6 +203,8 @@ abstract class BaseDataBaseTest {
             patientEntity = patientEntity,
             vaccineRecordCount = 2,
             testRecordCount = 2,
+            labTestCount = 0,
+            covidTestCount = 0,
             medicationRecordCount = 1
         )
 
@@ -211,6 +213,8 @@ abstract class BaseDataBaseTest {
             patientEntity = patientEntity,
             vaccineRecordCount = 0,
             testRecordCount = 1,
+            labTestCount = 0,
+            covidTestCount = 0,
             medicationRecordCount = 0
         )
 }
