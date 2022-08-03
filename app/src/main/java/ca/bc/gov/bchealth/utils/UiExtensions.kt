d99a604391bbf8560e935998a8f1095ae74a0bda
bsavoini-fw <bruno@freshworks.io>
2022-08-03 16:11:32 -0700

    HAPP-1007: Display helpBt on Add federal travel proof screen

--- app/src/main/java/ca/bc/gov/bchealth/utils/UiExtensions.kt
+++ app/src/main/java/ca/bc/gov/bchealth/utils/UiExtensions.kt
@@ -2,7 +2,19 @@ package ca.bc.gov.bchealth.utils
 
 import android.graphics.Paint
 import android.widget.TextView
+import ca.bc.gov.bchealth.R
+import com.google.android.material.appbar.MaterialToolbar
 
 fun TextView.underlineText() {
     this.paintFlags = this.paintFlags or Paint.UNDERLINE_TEXT_FLAG
+}
+
+fun MaterialToolbar.inflateHelpButton(action: () -> Unit) {
+    inflateMenu(R.menu.help_menu)
+    setOnMenuItemClickListener { menu ->
+        when (menu.itemId) {
+            R.id.menu_help -> action.invoke()
+        }
+        return@setOnMenuItemClickListener true
+    }
 }
