ff6a546fd7c4bc60a7ce8ede3ca611d72078b35e
bsavoini-fw <bruno@freshworks.io>
2022-09-06 14:04:06 -0700

    HAPP-912: Fix Recommendation field: diseaseEligibleDate

--- data/src/main/java/ca/bc/gov/data/datasource/remote/model/base/immunization/Immunization.kt
+++ data/src/main/java/ca/bc/gov/data/datasource/remote/model/base/immunization/Immunization.kt
@@ -1,9 +1,14 @@
 package ca.bc.gov.data.datasource.remote.model.base.immunization
 
+import com.google.gson.annotations.SerializedName
+
 /**
  * @author Pinakin Kansara
  */
 data class Immunization(
+    @SerializedName("name")
     val name: String?,
+
+    @SerializedName("immunizationAgents")
     val immunizationAgents: List<ImmunizationAgent> = emptyList()
 )
--- data/src/main/java/ca/bc/gov/data/datasource/remote/model/base/immunization/ImmunizationAgent.kt
+++ data/src/main/java/ca/bc/gov/data/datasource/remote/model/base/immunization/ImmunizationAgent.kt
@@ -1,11 +1,20 @@
 package ca.bc.gov.data.datasource.remote.model.base.immunization
 
+import com.google.gson.annotations.SerializedName
+
 /**
  * @author Pinakin Kansara
  */
 data class ImmunizationAgent(
+    @SerializedName("code")
     val code: String?,
+
+    @SerializedName("name")
     val name: String?,
+
+    @SerializedName("lotNumber")
     val lotNumber: String?,
+
+    @SerializedName("productName")
     val productName: String?
 )
