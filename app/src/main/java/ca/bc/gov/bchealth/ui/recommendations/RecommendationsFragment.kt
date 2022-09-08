81848cfaef796e3997ef8723e9db7a8f945d428b
bsavoini-fw <bruno@freshworks.io>
2022-09-08 16:35:42 -0700

    HAPP-912: Create recommendation item and display description

--- app/src/main/java/ca/bc/gov/bchealth/ui/recommendations/RecommendationsFragment.kt
+++ app/src/main/java/ca/bc/gov/bchealth/ui/recommendations/RecommendationsFragment.kt
@@ -1,15 +1,18 @@
 package ca.bc.gov.bchealth.ui.recommendations
 
+import android.os.Bundle
+import android.view.View
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import ca.bc.gov.bchealth.R
 import ca.bc.gov.bchealth.databinding.FragmentRecommendationsBinding
 import ca.bc.gov.bchealth.ui.BaseFragment
+import ca.bc.gov.bchealth.utils.setLinkSpannable
 import ca.bc.gov.bchealth.utils.viewBindings
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class RecommendationsFragment : BaseFragment(R.layout.fragment_resources)  {
+class RecommendationsFragment : BaseFragment(R.layout.fragment_recommendations) {
     private val binding by viewBindings(FragmentRecommendationsBinding::bind)
 
     override fun setToolBar(appBarConfiguration: AppBarConfiguration) {
@@ -33,5 +36,14 @@ class RecommendationsFragment : BaseFragment(R.layout.fragment_resources)  {
                 return@setOnMenuItemClickListener true
             }
         }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.tvDescription.setLinkSpannable(
+            getString(R.string.recommendations_description),
+            getString(R.string.recommendations_description_clickable),
+            getString(R.string.url_immunize_bc)
+        )
     }
 }
--- app/src/main/java/ca/bc/gov/bchealth/utils/UiExtensions.kt
+++ app/src/main/java/ca/bc/gov/bchealth/utils/UiExtensions.kt
@@ -3,7 +3,10 @@ package ca.bc.gov.bchealth.utils
 import android.graphics.Paint
 import android.text.Spannable
 import android.text.SpannableString
+import android.text.Spanned
+import android.text.method.LinkMovementMethod
 import android.text.style.ForegroundColorSpan
+import android.text.style.URLSpan
 import android.widget.TextView
 import androidx.annotation.ColorInt
 import ca.bc.gov.bchealth.R
@@ -28,16 +31,38 @@ fun TextView.setColorSpannable(
     coloredText: String,
     @ColorInt color: Int,
 ) {
+    setSpannable(fullContent, coloredText, color)
+}
+
+fun TextView.setLinkSpannable(
+    fullContent: String,
+    clickableText: String,
+    url : String,
+) {
+    val color = this.context.getColor(R.color.blue)
+    setSpannable(fullContent, clickableText, color, url)
+}
+
+
+private fun TextView.setSpannable(
+    fullContent: String,
+    coloredText: String,
+    @ColorInt color: Int,
+    url: String? = null
+) {
     val start = fullContent.indexOf(coloredText) // inclusive
     val end = start + coloredText.length // exclusive
 
     val spannable = SpannableString(fullContent)
+
+    if (url != null) {
+        spannable.setSpan(URLSpan(url), start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+    }
+
     spannable.setSpan(
-        ForegroundColorSpan(color),
-        start,
-        end,
-        Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        ForegroundColorSpan(color), start, end, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
     )
 
     this.text = spannable
+    this.movementMethod = LinkMovementMethod.getInstance() //to ensure invoke onClick
 }
