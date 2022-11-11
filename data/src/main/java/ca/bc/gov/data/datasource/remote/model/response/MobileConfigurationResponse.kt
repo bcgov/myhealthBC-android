cd8055f4b6857df45a13319848ed5c35703b5c60
bsavoini-fw <bruno@freshworks.io>
2022-11-10 16:39:50 -0800

    HAPP-1119: Map AuthenticationResponse

--- data/src/main/java/ca/bc/gov/data/datasource/remote/model/response/MobileConfigurationResponse.kt
+++ data/src/main/java/ca/bc/gov/data/datasource/remote/model/response/MobileConfigurationResponse.kt
@@ -1,10 +1,31 @@
 package ca.bc.gov.data.datasource.remote.model.response
 
+import com.google.gson.annotations.SerializedName
+
 /**
  * @author: Created by Rashmi Bambhania on 16,May,2022
  */
 data class MobileConfigurationResponse(
+    @SerializedName("online")
     val online: Boolean?,
+
+    @SerializedName("baseUrl")
     val baseUrl: String?,
+
+    @SerializedName("authentication")
+    val authentication: AuthenticationResponse,
+
+    @SerializedName("version")
     val version: Int,
+)
+
+data class AuthenticationResponse(
+    @SerializedName("endpoint")
+    val endpoint: String,
+
+    @SerializedName("identityProviderId")
+    val identityProviderId: String,
+
+    @SerializedName("clientId")
+    val clientId: String,
 )
