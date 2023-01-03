58702feeb0b76ad0aa374633a4042640fac4c4f1
bsavoini-fw <bruno@freshworks.io>
2023-01-03 09:44:08 -0800

    HAPP-925: Handle comments for Special Authority

--- app/src/main/java/ca/bc/gov/bchealth/ui/healthrecord/specialauthority/SpecialAuthorityDetailFragment.kt
+++ app/src/main/java/ca/bc/gov/bchealth/ui/healthrecord/specialauthority/SpecialAuthorityDetailFragment.kt
@@ -4,26 +4,26 @@ import android.os.Bundle
 import android.view.View
 import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.recyclerview.widget.ConcatAdapter
 import androidx.recyclerview.widget.LinearLayoutManager
 import ca.bc.gov.bchealth.R
 import ca.bc.gov.bchealth.databinding.FragmentSpecialAuthorityDetailBinding
-import ca.bc.gov.bchealth.ui.BaseFragment
+import ca.bc.gov.bchealth.ui.comment.CommentEntryTypeCode
+import ca.bc.gov.bchealth.ui.healthrecord.BaseRecordDetailFragment
 import ca.bc.gov.bchealth.utils.viewBindings
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
-class SpecialAuthorityDetailFragment : BaseFragment(R.layout.fragment_special_authority_detail) {
+class SpecialAuthorityDetailFragment :
+    BaseRecordDetailFragment(R.layout.fragment_special_authority_detail) {
 
     private val binding by viewBindings(FragmentSpecialAuthorityDetailBinding::bind)
     private val args: SpecialAuthorityDetailFragmentArgs by navArgs()
     private val viewModel: SpecialAuthorityDetailViewModel by viewModels()
+    private lateinit var concatAdapter: ConcatAdapter
     private lateinit var specialAuthorityAdapter: SpecialAuthorityAdapter
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -31,24 +31,32 @@ class SpecialAuthorityDetailFragment : BaseFragment(R.layout.fragment_special_au
         setUpRecyclerView()
         observeUiState()
         viewModel.getSpecialAuthorityDetails(args.specialAuthorityId)
+        initComments()
     }
+
+    override fun getCommentEntryTypeCode() = CommentEntryTypeCode.SPECIAL_AUTHORITY
+
+    override fun getParentEntryId() = viewModel.uiState.value.parentEntryId
+
+    override fun getCommentView() = binding.viewComment
 
     private fun observeUiState() {
-        viewLifecycleOwner.lifecycleScope.launch {
-            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
-                viewModel.uiState.collect { state ->
+        viewModel.uiState.collectOnStart { state ->
 
-                    binding.progressBar.isVisible = state.onLoading
+            binding.progressBar.isVisible = state.onLoading
+            binding.layoutToolbar.topAppBar.title = state.toolbarTitle
 
-                    binding.layoutToolbar.topAppBar.title = state.toolbarTitle
-
-                    if (state.specialAuthorityDetailItems.isNotEmpty()) {
-                        specialAuthorityAdapter.submitList(state.specialAuthorityDetailItems)
-                    }
-                }
+            if (state.specialAuthorityDetailItems.isNotEmpty()) {
+                specialAuthorityAdapter.submitList(state.specialAuthorityDetailItems)
             }
+
+            getComments(state.parentEntryId)
         }
     }
+
+    override fun getProgressBar() = binding.progressBar
+
+    override fun getScrollableView() = binding.rvSpecialAuthorityDetails
 
     override fun setToolBar(appBarConfiguration: AppBarConfiguration) {
         with(binding.layoutToolbar.topAppBar) {
@@ -61,8 +69,10 @@ class SpecialAuthorityDetailFragment : BaseFragment(R.layout.fragment_special_au
 
     private fun setUpRecyclerView() {
         specialAuthorityAdapter = SpecialAuthorityAdapter()
+        concatAdapter = ConcatAdapter(specialAuthorityAdapter, getRecordCommentsAdapter())
+
         val recyclerView = binding.rvSpecialAuthorityDetails
-        recyclerView.adapter = specialAuthorityAdapter
+        recyclerView.adapter = concatAdapter
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
     }
 }
