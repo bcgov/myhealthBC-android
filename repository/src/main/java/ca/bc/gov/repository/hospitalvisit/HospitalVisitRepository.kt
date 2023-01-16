e472c341f14944ab7f67e1dec57aa134ae55acb6
bsavoini-fw <bruno@freshworks.io>
2023-01-16 11:37:05 -0800

    [HAPP-1266] Refactor worker
    Creates generic fetchRecord method and extract inserts methods

--- repository/src/main/java/ca/bc/gov/repository/hospitalvisit/HospitalVisitRepository.kt
+++ repository/src/main/java/ca/bc/gov/repository/hospitalvisit/HospitalVisitRepository.kt
@@ -1,5 +1,6 @@
 package ca.bc.gov.repository.hospitalvisit
 
+import ca.bc.gov.common.model.hospitalvisits.HospitalVisitDto
 import ca.bc.gov.data.datasource.local.HospitalVisitLocalDataSource
 import ca.bc.gov.data.datasource.remote.HospitalVisitRemoteDataSource
 import javax.inject.Inject
@@ -8,6 +9,12 @@ class HospitalVisitRepository @Inject constructor(
     private val localDataSource: HospitalVisitLocalDataSource,
     private val remoteDataSource: HospitalVisitRemoteDataSource
 ) {
+
+    suspend fun deleteHospitalVisitsDto(patientId: Long) {
+    }
+
+    suspend fun insertHospitalVisits(list: List<HospitalVisitDto>) {
+    }
 
     suspend fun getHospitalVisits(token: String, hdid: String) =
         remoteDataSource.getHospitalVisit(token, hdid)
