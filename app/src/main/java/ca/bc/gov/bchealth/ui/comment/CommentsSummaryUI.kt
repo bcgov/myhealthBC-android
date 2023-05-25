cd1f1eef8587c85c4d724c6b6a298c514d8876ed
bsavoini-fw <bruno@freshworks.io>
2023-05-25 11:57:55 -0700

    HAPP-1486: Convert Banner to Compose

--- app/src/main/java/ca/bc/gov/bchealth/ui/comment/CommentsSummaryUI.kt
+++ app/src/main/java/ca/bc/gov/bchealth/ui/comment/CommentsSummaryUI.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.pluralStringResource
