ee0fa8fd89f59eeab499e2770c00f0bc69344849
bsavoini-fw <bruno@freshworks.io>
2023-05-29 12:09:57 -0700

    Revert "Revert "Patch - Restricting browser""
    This reverts commit b5d58ebf70b34326f6746ffb619aab5f3c1f2485.

--- repository/src/main/java/ca/bc/gov/repository/bcsc/BcscAuthExtension.kt
+++ repository/src/main/java/ca/bc/gov/repository/bcsc/BcscAuthExtension.kt
@@ -5,12 +5,18 @@ import android.net.Uri
 import ca.bc.gov.common.const.AUTH_ERROR
 import ca.bc.gov.common.const.AUTH_ERROR_DO_LOGIN
 import ca.bc.gov.common.exceptions.MyHealthException
+import ca.bc.gov.repository.library.java.net.openid.appauth.AppAuthConfiguration
 import ca.bc.gov.repository.library.java.net.openid.appauth.AuthState
 import ca.bc.gov.repository.library.java.net.openid.appauth.AuthorizationException
 import ca.bc.gov.repository.library.java.net.openid.appauth.AuthorizationResponse
 import ca.bc.gov.repository.library.java.net.openid.appauth.AuthorizationService
 import ca.bc.gov.repository.library.java.net.openid.appauth.AuthorizationServiceConfiguration
 import ca.bc.gov.repository.library.java.net.openid.appauth.TokenResponse
+import ca.bc.gov.repository.library.java.net.openid.appauth.browser.BrowserAllowList
+import ca.bc.gov.repository.library.java.net.openid.appauth.browser.BrowserDenyList
+import ca.bc.gov.repository.library.java.net.openid.appauth.browser.Browsers
+import ca.bc.gov.repository.library.java.net.openid.appauth.browser.VersionRange
+import ca.bc.gov.repository.library.java.net.openid.appauth.browser.VersionedBrowserMatcher
 import kotlinx.coroutines.suspendCancellableCoroutine
 import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
@@ -39,7 +45,25 @@ suspend fun awaitPerformTokenRequest(
     authorizationResponse: AuthorizationResponse
 ): Pair<TokenResponse, AuthorizationException?> = suspendCancellableCoroutine { continuation ->
 
-    AuthorizationService(applicationContext).performTokenRequest(
+    val appauthConfiguration = AppAuthConfiguration.Builder()
+        .setBrowserMatcher(
+            BrowserAllowList(
+                VersionedBrowserMatcher.CHROME_CUSTOM_TAB,
+                VersionedBrowserMatcher.SAMSUNG_CUSTOM_TAB
+            )
+        )
+        .setBrowserMatcher(
+            BrowserDenyList(
+                VersionedBrowserMatcher(
+                    Browsers.SBrowser.PACKAGE_NAME,
+                    Browsers.SBrowser.SIGNATURE_SET,
+                    true,
+                    VersionRange.atMost("5.3")
+                )
+            )
+        )
+        .build()
+    AuthorizationService(applicationContext, appauthConfiguration).performTokenRequest(
         authorizationResponse.createTokenExchangeRequest()
     ) { response, ex ->
         if (ex != null || response == null) {
@@ -54,7 +78,25 @@ suspend fun awaitPerformActionWithFreshTokens(
     applicationContext: Context,
     authState: AuthState
 ): String = suspendCancellableCoroutine { continuation ->
-    val authService = AuthorizationService(applicationContext)
+    val appauthConfiguration = AppAuthConfiguration.Builder()
+        .setBrowserMatcher(
+            BrowserAllowList(
+                VersionedBrowserMatcher.CHROME_CUSTOM_TAB,
+                VersionedBrowserMatcher.SAMSUNG_CUSTOM_TAB
+            )
+        )
+        .setBrowserMatcher(
+            BrowserDenyList(
+                VersionedBrowserMatcher(
+                    Browsers.SBrowser.PACKAGE_NAME,
+                    Browsers.SBrowser.SIGNATURE_SET,
+                    true,
+                    VersionRange.atMost("5.3")
+                )
+            )
+        )
+        .build()
+    val authService = AuthorizationService(applicationContext, appauthConfiguration)
     authState.performActionWithFreshTokens(
         authService
     ) { accessToken, idToken, ex ->
