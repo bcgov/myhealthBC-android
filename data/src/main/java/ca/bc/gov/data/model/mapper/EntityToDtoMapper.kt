e18bb72c89d4df452b9abe8b8c68305c6898c47b
bsavoini-fw <bruno@freshworks.io>
2023-06-08 12:40:18 -0700

    HAPP-1493: API calls to notification deletion

--- data/src/main/java/ca/bc/gov/data/model/mapper/EntityToDtoMapper.kt
+++ data/src/main/java/ca/bc/gov/data/model/mapper/EntityToDtoMapper.kt
@@ -465,7 +465,7 @@ fun DiagnosticImagingDataEntity.toDto() = DiagnosticImagingDataDto(
 )
 
 fun NotificationEntity.toDto() = NotificationDto(
-    id = notificationId,
+    notificationId = notificationId,
     hdid = hdid,
     category = category,
     displayText = displayText,
