bd9b9a060fe6129c74ca51a084a36ba9a8994f96
bsavoini-fw <bruno@freshworks.io>
2023-06-09 16:27:29 -0700

    HAPP-1493: Handle session expiration

--- app/src/main/java/ca/bc/gov/bchealth/ui/notification/NotificationScreen.kt
+++ app/src/main/java/ca/bc/gov/bchealth/ui/notification/NotificationScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -44,7 +45,7 @@ fun NotificationScreen(
     onClickAction: (NotificationViewModel.NotificationItem) -> Unit
 ) {
 
-    Box {
+    Box(Modifier.fillMaxSize()) {
         LazyColumn(Modifier.padding(horizontal = 32.dp)) {
             items(uiState.list) {
                 Text(
