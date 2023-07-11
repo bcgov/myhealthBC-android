2df2bc3eceebef678fa250628597c704da8be9bb
bsavoini-fw <bruno@freshworks.io>
2023-07-11 14:30:22 -0700

    HAPP-1539: Remove QuickAccessTileEntity

--- data/src/main/java/ca/bc/gov/data/datasource/local/AppFeatureLocalDataSource.kt
+++ data/src/main/java/ca/bc/gov/data/datasource/local/AppFeatureLocalDataSource.kt
@@ -1,7 +1,6 @@
 package ca.bc.gov.data.datasource.local
 
 import ca.bc.gov.common.model.settings.AppFeatureDto
-import ca.bc.gov.common.model.settings.AppFeatureWithQuickAccessTilesDto
 import ca.bc.gov.data.datasource.local.dao.AppFeatureDao
 import ca.bc.gov.data.model.mapper.toDto
 import ca.bc.gov.data.model.mapper.toEntity
@@ -15,7 +14,9 @@ class AppFeatureLocalDataSource @Inject constructor(
         return appFeatureDao.insert(appFeatureDto.toEntity())
     }
 
-    suspend fun getAppFeaturesWithQuickAccessTiles(): List<AppFeatureWithQuickAccessTilesDto> {
-        return appFeatureDao.getAllFeatureWithQuickAccessTiles().map { it.toDto() }
+    suspend fun getAppFeaturesWithQuickAccessTiles(): List<AppFeatureDto> {
+        return appFeatureDao.getAllFeatureWithQuickAccessTiles().map {
+            it.toDto()
+        }
     }
 }
