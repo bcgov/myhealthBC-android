2215b707317714535f33ed2fd9495e156d4d3b52
bsavoini-fw <bruno@freshworks.io>
2023-07-12 14:44:50 -0700

    HAPP-1539: Update QuickAccess flag

--- data/src/main/java/ca/bc/gov/data/datasource/local/dao/AppFeatureDao.kt
+++ data/src/main/java/ca/bc/gov/data/datasource/local/dao/AppFeatureDao.kt
@@ -12,8 +12,14 @@ interface AppFeatureDao {
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(appFeatureEntity: AppFeatureEntity): Long
 
+    @Query("UPDATE app_feature SET quick_access_enabled = :enabled WHERE ID = :appFeatureId")
+    suspend fun updateQuickAccessFlag(appFeatureId: Long, enabled: Boolean)
+
     @Query("DELETE FROM app_feature")
     suspend fun deleteAll()
+
+    @Query("SELECT count(*) FROM app_feature")
+    suspend fun countRegisters(): Int
 
     @Query("SELECT * FROM app_feature WHERE quick_access_enabled = 1 ")
     suspend fun getQuickAccessTiles(): List<AppFeatureEntity>
