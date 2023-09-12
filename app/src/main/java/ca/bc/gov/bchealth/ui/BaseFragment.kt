e482bb15fcf5e15b2b14e1f23057167b4b0b2888
bsavoini-fw <bruno@freshworks.io>
2023-09-12 15:14:20 -0700

    HAPP-1597: Display LabResults in Compose

--- app/src/main/java/ca/bc/gov/bchealth/ui/BaseFragment.kt
+++ app/src/main/java/ca/bc/gov/bchealth/ui/BaseFragment.kt
@@ -99,13 +99,13 @@ abstract class BaseFragment(@LayoutRes private val contentLayoutId: Int?) : Frag
 
     private fun resetBaseUiState() = getBaseViewModel()?.resetBaseUiState()
 
-    private fun showServiceDownMessage() {
+    fun showServiceDownMessage() {
         view?.let {
             it.showServiceDownMessage(it.context)
         }
     }
 
-    private fun showNoInternetConnectionMessage() {
+    fun showNoInternetConnectionMessage() {
         view?.let {
             it.showNoInternetConnectionMessage(it.context)
         }
--- app/src/main/java/ca/bc/gov/bchealth/ui/healthrecord/labtest/LabTestDetailFragment.kt
+++ app/src/main/java/ca/bc/gov/bchealth/ui/healthrecord/labtest/LabTestDetailFragment.kt
@@ -3,38 +3,39 @@ package ca.bc.gov.bchealth.ui.healthrecord.labtest
 import android.os.Bundle
 import android.view.View
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.core.os.bundleOf
-import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
-import androidx.recyclerview.widget.ConcatAdapter
+import androidx.work.WorkInfo
 import ca.bc.gov.bchealth.R
-import ca.bc.gov.bchealth.databinding.FragmentLabTestDetailBinding
+import ca.bc.gov.bchealth.ui.BaseFragment
 import ca.bc.gov.bchealth.ui.comment.CommentEntryTypeCode
-import ca.bc.gov.bchealth.ui.healthrecord.BaseRecordDetailFragment
+import ca.bc.gov.bchealth.ui.comment.CommentsSummary
+import ca.bc.gov.bchealth.ui.comment.CommentsUiState
+import ca.bc.gov.bchealth.ui.comment.CommentsViewModel
+import ca.bc.gov.bchealth.ui.custom.MyHealthScaffold
 import ca.bc.gov.bchealth.utils.AlertDialogHelper
 import ca.bc.gov.bchealth.utils.PdfHelper
-import ca.bc.gov.bchealth.utils.showNoInternetConnectionMessage
-import ca.bc.gov.bchealth.utils.showServiceDownMessage
-import ca.bc.gov.bchealth.utils.viewBindings
+import ca.bc.gov.bchealth.utils.observeWork
+import ca.bc.gov.bchealth.utils.redirect
 import ca.bc.gov.bchealth.viewmodel.PdfDecoderViewModel
-import ca.bc.gov.bchealth.widget.AddCommentLayout
+import ca.bc.gov.common.BuildConfig
+import ca.bc.gov.repository.SYNC_COMMENTS
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 import java.io.File
 
 @AndroidEntryPoint
-class LabTestDetailFragment : BaseRecordDetailFragment(R.layout.fragment_lab_test_detail) {
-
-    private val binding by viewBindings(FragmentLabTestDetailBinding::bind)
+class LabTestDetailFragment : BaseFragment(null) {
     private val viewModel: LabTestDetailViewModel by viewModels()
+    private val commentsViewModel: CommentsViewModel by viewModels()
 
-    private lateinit var labTestDetailAdapter: LabTestDetailAdapter
-    private lateinit var concatAdapter: ConcatAdapter
     private val args: LabTestDetailFragmentArgs by navArgs()
     private val pdfDecoderViewModel: PdfDecoderViewModel by viewModels()
     private var fileInMemory: File? = null
@@ -44,67 +45,99 @@ class LabTestDetailFragment : BaseRecordDetailFragment(R.layout.fragment_lab_tes
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setupComposeToolbar(binding.composeToolbar.root)
-        setUpRecyclerView(args.hdid)
-        viewModel.getLabTestDetails(args.labOrderId)
-        observeUiState()
         observePdfData()
-        initComments()
+        observeCommentsSyncCompletion()
     }
 
-    override fun getCommentView(): AddCommentLayout = binding.comment
-
-    override fun getScrollableView() = binding.rvLabTestDetailList
-
-    override fun getCommentEntryTypeCode() = CommentEntryTypeCode.LAB_RESULTS
-
-    override fun getParentEntryId(): String? = viewModel.uiState.value.parentEntryId
-
-    override fun getProgressBar(): View = binding.progressBar
-
-    private fun setUpRecyclerView(hdid: String?) {
-        labTestDetailAdapter = LabTestDetailAdapter(
-            viewPdfClickListener = { viewModel.getLabTestPdf(hdid) }
-        )
-
-        concatAdapter = ConcatAdapter(labTestDetailAdapter, getRecordCommentsAdapter())
-
-        binding.rvLabTestDetailList.adapter = concatAdapter
+    override fun onResume() {
+        super.onResume()
+        viewModel.getLabTestDetails(args.labOrderId)
     }
 
-    private fun observeUiState() {
-        viewModel.uiState.collectOnStart { state ->
-            binding.progressBar.isVisible = state.onLoading
+    @Composable
+    override fun GetComposableLayout() {
 
-            handledServiceDown(state)
+        val uiState = viewModel.uiState.collectAsState().value
 
-            if (state.labTestDetails?.isNotEmpty() == true) {
-                labTestDetailAdapter.submitList(state.labTestDetails)
-                setupComposeToolbar(binding.composeToolbar.root, state.toolbarTitle)
+        var commentState: CommentsUiState? = null
+
+        if (BuildConfig.FLAG_ADD_COMMENTS) {
+            uiState.parentEntryId?.let { commentsViewModel.getComments(it) }
+            commentState = commentsViewModel.uiState.collectAsState().value
+        }
+
+        MyHealthScaffold(
+            title = uiState.toolbarTitle,
+            isLoading = uiState.onLoading,
+            navigationAction = { findNavController().popBackStack() }
+        ) {
+            LabTestScreen(
+                uiState = uiState,
+                onClickViewPdf = { viewModel.getLabTestPdf(args.hdid) },
+                onClickFaq = { context?.redirect(getString(R.string.faq_link)) },
+                onClickComments = ::navigateToComments,
+                commentsSummary = commentState?.commentsSummary,
+                onSubmitComment = ::onSubmitComment,
+            )
+        }
+        handledServiceDown(uiState)
+
+        if (uiState.onError) {
+            showError()
+            viewModel.resetUiState()
+        }
+
+        handlePdfDownload(uiState)
+
+        handleNoInternetConnection(uiState)
+
+        if (commentState?.isBcscSessionActive == false) findNavController().popBackStack()
+    }
+
+    private fun observeCommentsSyncCompletion() {
+        if (BuildConfig.FLAG_ADD_COMMENTS.not()) return
+
+        observeWork(SYNC_COMMENTS) {
+            if (it == WorkInfo.State.SUCCEEDED) {
+                getParentEntryId()?.let { parentId ->
+                    commentsViewModel.getComments(parentId)
+                }
             }
-
-            if (state.onError) {
-                showError()
-                viewModel.resetUiState()
-            }
-
-            handlePdfDownload(state)
-
-            handleNoInternetConnection(state)
-            getComments(state.parentEntryId)
         }
     }
 
+    private fun onSubmitComment(content: String) {
+        getParentEntryId()?.let { parentEntryId ->
+            commentsViewModel.addComment(
+                parentEntryId,
+                content,
+                CommentEntryTypeCode.LAB_RESULTS.value,
+            )
+        }
+    }
+
+    private fun navigateToComments(commentsSummary: CommentsSummary) {
+        findNavController().navigate(
+            R.id.commentsFragment,
+            bundleOf(
+                "parentEntryId" to commentsSummary.parentEntryId,
+                "recordType" to commentsSummary.entryTypeCode,
+            )
+        )
+    }
+
+    private fun getParentEntryId(): String? = viewModel.uiState.value.parentEntryId
+
     private fun handledServiceDown(state: LabTestDetailUiState) {
         if (!state.isHgServicesUp) {
-            binding.root.showServiceDownMessage(requireContext())
+            showServiceDownMessage()
             viewModel.resetUiState()
         }
     }
 
     private fun handleNoInternetConnection(uiState: LabTestDetailUiState) {
         if (!uiState.isConnected) {
-            binding.root.showNoInternetConnectionMessage(requireContext())
+            showNoInternetConnectionMessage()
             viewModel.resetUiState()
         }
     }
