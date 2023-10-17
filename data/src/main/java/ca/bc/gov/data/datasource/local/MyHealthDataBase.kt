110561bfc2b2cfbe953d183f5b27c8c795dd5999
PINAKIN-KANSARA-EY <Pinakin.Kansara@ca.ey.com>
2023-10-17 16:33:41 -0700

    HAPP-1637 Pharmacist Assessment - added option to show pharmacist assessment.

--- data/src/main/java/ca/bc/gov/data/datasource/local/MyHealthDataBase.kt
+++ data/src/main/java/ca/bc/gov/data/datasource/local/MyHealthDataBase.kt
@@ -70,7 +70,7 @@ import ca.bc.gov.data.datasource.local.entity.userprofile.UserProfileEntity
  * @author Pinakin Kansara
  */
 @Database(
-    version = 16,
+    version = 17,
     entities = [
         PatientEntity::class,
         VaccineRecordEntity::class,
@@ -107,7 +107,8 @@ import ca.bc.gov.data.datasource.local.entity.userprofile.UserProfileEntity
         AutoMigration(from = 12, to = 13),
         AutoMigration(from = 13, to = 14),
         AutoMigration(from = 14, to = 15, spec = MyHealthDataBase.AppFeatureMigration::class),
-        AutoMigration(from = 15, to = 16)
+        AutoMigration(from = 15, to = 16),
+        AutoMigration(from = 16, to = 17)
     ],
     exportSchema = true
 )
