e2a46e5c91cef23fcb9b73aaa6e44eda370a6fde
PINAKIN-KANSARA-EY <Pinakin.Kansara@ca.ey.com>
2024-01-30 12:20:48 -0800

    HAPP-1824 - fixed hidden medication - fixed progress indicator for the clinical document detail pdf fetch.

--- app/src/main/java/ca/bc/gov/bchealth/ui/healthrecord/clinicaldocument/ClinicalDocumentDetailScreen.kt
+++ app/src/main/java/ca/bc/gov/bchealth/ui/healthrecord/clinicaldocument/ClinicalDocumentDetailScreen.kt
@@ -1,6 +1,7 @@
 package ca.bc.gov.bchealth.ui.healthrecord.clinicaldocument
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,10 +9,13 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
@@ -83,42 +87,53 @@ private fun ClinicalDocumentDetailScreenContent(
     uiState: ClinicalDocumentUiState,
     commentUIState: CommentsUiState
 ) {
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+    ) {
 
-    Column(modifier = modifier.fillMaxSize()) {
-        LazyColumn(
-            modifier.weight(1F), contentPadding = PaddingValues(32.dp),
-            verticalArrangement = Arrangement.spacedBy(16.dp)
-        ) {
-            item {
-
-                HGLargeOutlinedButton(
-                    onClick = onClickDownload,
-                    modifier = Modifier
-                        .fillMaxWidth(),
-                    text = stringResource(id = R.string.clinical_documents_detail_button_download)
-                )
-            }
-
-            items(uiState.uiList) { listItem ->
-                HealthRecordListItem(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .wrapContentHeight(),
-                    stringResource(id = listItem.title),
-                    listItem.description.orEmpty(),
-                )
-            }
-        }
-
-        if (BuildConfig.FLAG_ADD_COMMENTS) {
-            CommentsSummaryUI(
-                onClickComments = onClickComments,
-                commentsSummary = commentUIState.commentsSummary
+        if (uiState.onLoading) {
+            CircularProgressIndicator(
+                modifier = modifier.align(Alignment.Center),
+                color = MaterialTheme.colors.primary
             )
         }
+        Column(modifier = modifier.fillMaxSize()) {
+            LazyColumn(
+                modifier.weight(1F), contentPadding = PaddingValues(32.dp),
+                verticalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                item {
 
-        if (BuildConfig.FLAG_ADD_COMMENTS) {
-            CommentInputUI(onSubmitComment = onSubmitComment)
+                    HGLargeOutlinedButton(
+                        onClick = onClickDownload,
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        text = stringResource(id = R.string.clinical_documents_detail_button_download)
+                    )
+                }
+
+                items(uiState.uiList) { listItem ->
+                    HealthRecordListItem(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .wrapContentHeight(),
+                        stringResource(id = listItem.title),
+                        listItem.description.orEmpty(),
+                    )
+                }
+            }
+
+            if (BuildConfig.FLAG_ADD_COMMENTS) {
+                CommentsSummaryUI(
+                    onClickComments = onClickComments,
+                    commentsSummary = commentUIState.commentsSummary
+                )
+            }
+
+            if (BuildConfig.FLAG_ADD_COMMENTS) {
+                CommentInputUI(onSubmitComment = onSubmitComment)
+            }
         }
     }
 }
