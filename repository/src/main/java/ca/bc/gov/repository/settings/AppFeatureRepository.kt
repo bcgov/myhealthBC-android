44bbd9e89035781080c7d6dc021dac67bc47d395
PINAKIN-KANSARA-EY <Pinakin.Kansara@ca.ey.com>
2024-02-07 15:00:09 -0800

    HAPP-1844 - fixed issue with Cancer Banner - added BC Cancer Screening to quick link

--- repository/src/main/java/ca/bc/gov/repository/settings/AppFeatureRepository.kt
+++ repository/src/main/java/ca/bc/gov/repository/settings/AppFeatureRepository.kt
@@ -1,7 +1,11 @@
 package ca.bc.gov.repository.settings
 
+import ca.bc.gov.common.const.DATABASE_ERROR
+import ca.bc.gov.common.exceptions.MyHealthException
+import ca.bc.gov.common.model.AppFeatureName
 import ca.bc.gov.common.model.settings.AppFeatureDto
 import ca.bc.gov.data.datasource.local.AppFeatureLocalDataSource
+import ca.bc.gov.data.model.mapper.toDto
 import javax.inject.Inject
 
 class AppFeatureRepository @Inject constructor(
@@ -13,4 +17,8 @@ class AppFeatureRepository @Inject constructor(
     }
 
     suspend fun getAppFeaturesWithQuickAccessTiles() = appFeatureLocalDataSource.getAppFeaturesWithQuickAccessTiles()
+
+    suspend fun getAppFeature(name: AppFeatureName): AppFeatureDto = appFeatureLocalDataSource.getAppFeature(name)?.toDto() ?: throw MyHealthException(
+        DATABASE_ERROR, message = "app feature not found"
+    )
 }
